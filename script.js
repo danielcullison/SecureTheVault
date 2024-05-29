673bfde4c378e8109f8adaf45197176d66d8650b
Daniel Cullison <danielcullison@Daniels-Air.attlocal.net>
2024-05-28 19:59:17 -0500

    Final Version

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/* 
+Step by Step:
+-Create a user message variable that contains the string for the user
+-Create three variables, each equaling one of the numbers in the vault combination. Use a different arithmetic operator for each variable.
+-Create a template literal that includes the user message and the three variables.
+-Use the alert method to display this template literal.
+*/
+
 // This is the user message
 userMessage = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination : ';
 
