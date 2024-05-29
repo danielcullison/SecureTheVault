643956fa47db67bb87f059dc8916c04666f96e27
Daniel Cullison <danielcullison@Daniels-Air.attlocal.net>
2024-05-28 19:43:53 -0500

    Final Version

---
+++ script.js
@@ -0,0 +1,11 @@
+// This is the user message
+userMessage = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination : ';
+
+// These are the three variables used to get the combination code
+const variableOne = 5 * 2;
+const variableTwo = 80 / 2;
+const variableThree = 30 + 9;
+
+// This is the template literal used to display the user message with the vault combination
+alert(`${userMessage} ${variableOne} - ${variableTwo} - ${variableThree}`);
+
